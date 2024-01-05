38f334080265c5b754bd867fbde87285d5b10633
Dreamy Cecil <stalker27production@gmail.com>
2024-01-05 04:17:03 +0300

    [Classic Patch] Replace strlen() with CTString::Length() where applicable.

--- Sources/SeriousSam/GUI/Components/MGEdit.cpp
+++ Sources/SeriousSam/GUI/Components/MGEdit.cpp
@@ -42,7 +42,7 @@ void CMGEdit::OnActivate(void) {
   ASSERT(mg_pstrToChange != NULL);
   PlayMenuSound(_psdPress);
   IFeel_PlayEffect("Menu_press");
-  mg_iCursorPos = strlen(GetText());
+  mg_iCursorPos = GetText().Length();
   mg_bEditing = TRUE;
   _bEditingString = TRUE;
 }
@@ -61,7 +61,7 @@ void CMGEdit::OnKillFocus(void) {
 // helper function for deleting char(s) from string
 static void Key_BackDel(CTString &str, INDEX &iPos, BOOL bShift, BOOL bRight) {
   // do nothing if string is empty
-  INDEX ctChars = strlen(str);
+  INDEX ctChars = str.Length();
 
   if (ctChars == 0) {
     return;
@@ -123,7 +123,7 @@ BOOL CMGEdit::OnKeyDown(int iVKey) {
     } break;
 
     case VK_RIGHT: {
-      if (mg_iCursorPos < strlen(GetText())) {
+      if (mg_iCursorPos < GetText().Length()) {
         mg_iCursorPos++;
       }
     } break;
@@ -133,7 +133,7 @@ BOOL CMGEdit::OnKeyDown(int iVKey) {
     } break;
 
     case VK_END: {
-      mg_iCursorPos = strlen(GetText());
+      mg_iCursorPos = GetText().Length();
     } break;
 
     case VK_BACK: {
